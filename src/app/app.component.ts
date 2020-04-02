f5aa4184588dbc25b2a169367816c859be0300d9
ivenzhyk <ivenzhyk@deloitte.com>
2020-04-02 19:19:33 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wlt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'whitelist-tube';
+}
